a49f24ab7743d32230bb248298a6b8f9070eb5c3
kxw1975 <kxw1975@rit.edu>
2017-04-25 22:20:14 -0400

    -updated shader, will test later

--- DownsamplerPS.hlsl
+++ DownsamplerPS.hlsl
@@ -1,6 +1,12 @@
 Texture2D tex	: register(t0);
 SamplerState texSampler	: register(s0);
 
+cbuffer Data : register(b0)
+{
+	float texelHeight;
+	float texelWidth;
+};
+
 
 struct TargetCoords
 {
@@ -10,25 +16,31 @@ struct TargetCoords
 
 
 struct SizedOutput {
-	float4 quarterSize	: SV_Target0;
+	float4 halfSize	: SV_Target0;
+	float4 quarterSize	: SV_Target1;
 };
 
 SizedOutput main(TargetCoords input)
 {
 	SizedOutput output;
-	float texelHeight = 1 / 720;
-	float texelWidth = 1 / 1280;
+
 	float4 col = float4(0, 0, 0, 0);
-	//doesn't output a texture downsized, only the top left corner of the full texture --> TODO: output full texture downsized --> method?
-	//0,0 is drawing 0,0 and pixel to right and down
-	// 0.5,0.5 is drawing 1,1
-	//all must shift over by a multiple of some value so that the whole tex is sampled for the smaller one? Or is it that this is not sampling the correct values <-- this i think
-	col += tex.Sample(texSampler, input.uv + float2(0.0f, texelHeight));
-	col += tex.Sample(texSampler, input.uv + float2(texelWidth, 0.0f));
-	col += tex.Sample(texSampler, input.uv);
-	col = output.quarterSize / 3;
+	col += tex.Sample(texSampler, (input.uv * 2) + float2(0.0f, -texelHeight));
+	col += tex.Sample(texSampler, (input.uv * 2) + float2(-texelWidth, -texelHeight));
+	col += tex.Sample(texSampler, (input.uv * 2) + float2(-texelWidth, 0.0f);
+	col += tex.Sample(texSampler, (input.uv * 2));
+	col = output.quarterSize / 4;
 	col.a = 1.0f;
-	output.quarterSize = col;
+	output.halfSize = col;
+
+	float4 qCol = float4(0, 0, 0, 0);
+	qCol += tex.Sample(texSampler, (input.uv * 4) + float2(0.0f, -texelHeight));
+	qCol += tex.Sample(texSampler, (input.uv * 4) + float2(-texelWidth, -texelHeight));
+	qCol += tex.Sample(texSampler, (input.uv * 4) + float2(-texelWidth, 0.0f);
+	qCol += tex.Sample(texSampler, (input.uv * 4));
+	qCol = output.quarterSize / 4;
+	qCol.a = 1.0f;
+	output.quarterSize = qCol;
 	//output.quarterSize = tex.Sample(texSampler, input.uv);
 	return output;
 }
